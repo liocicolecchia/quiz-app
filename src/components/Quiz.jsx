1692d45acf6c81d94ca857bba5d0cdc8e8b9f875
Lio <liocicolecchia@gmail.com>
2026-06-02 20:02:10 +0200

    adding logic to Quiz component / questions and matching answers get displayed and answers button functionalaty added

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -7,7 +7,30 @@ function Quiz() {
 
   const activeQuestionIndex = userAnswers.length;
 
-  return <h2>{QUESTIONS[activeQuestionIndex].text}</h2>;
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => {
+            return (
+              <li key={answer} className="answer">
+                <button onClick={() => handleSelectAnswer(answer)}>
+                  {answer}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Quiz;
